0d474b8285e0048c88831da6d2cf38d561757146
ns5xq2mqtf-del <tkhsyrk811@gmail.com>
2026-03-24 00:06:18 +0900

    検索バー用ビュー作成

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,2 +1,9 @@
 class ContentsController < ApplicationController
+    def index
+        @contents = Content.all
+        if params[:keyword].present?
+            #contentモデルのtitle/genre/authorカラムを対象にあいまい検索。
+            @contents = @contents.search_all_columns(params[:keyword])
+        end
+    end
 end
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,8 @@
 class Content < ApplicationRecord
+    scope :search_all_columns, ->(word)
+    columns = ["title", "genre", "author"]
+    # .mapで配列の要素から～LIKE?文へ変形。.joinで文間にOR挿入。
+    query = columns.map { |c| "#{c} LIKE ? "}.join(" OR ")
+    # where(SQL文,~,~)のかたちに持ってきたい。
+    where(query, *Array.new(columns.size, "%#{word}%"))
 end
