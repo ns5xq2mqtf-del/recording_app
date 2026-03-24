1a036a147356133b671a4777cb837b646495d06b
ns5xq2mqtf-del <tkhsyrk811@gmail.com>
2026-03-24 22:22:14 +0900

    モデルにhas_one_attached追記

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,8 +1,11 @@
 class Content < ApplicationRecord
-    scope :search_all_columns, ->(word)
-    columns = ["title", "genre", "author"]
-    # .mapで配列の要素から～LIKE?文へ変形。.joinで文間にOR挿入。
-    query = columns.map { |c| "#{c} LIKE ? "}.join(" OR ")
-    # where(SQL文,~,~)のかたちに持ってきたい。
-    where(query, *Array.new(columns.size, "%#{word}%"))
+    has_one_attached :content_image
+
+    scope :search_all_columns, ->(word) do
+        columns = ["title", "genre", "author"]
+        # .mapで配列の要素から～LIKE?文へ変形。.joinで文間にOR挿入。
+        query = columns.map { |c| "#{c} LIKE ? "}.join(" OR ")
+        # where(SQL文,~,~)のかたちに持ってきたい。
+        where(query, *Array.new(columns.size, "%#{word}%"))
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :contents, dependent: :destroy
+
+  has_one_attached :avatar
 end
