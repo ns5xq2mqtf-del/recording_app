9de48006f2260317c173161096ecdbae29ed266e
ns5xq2mqtf-del <tkhsyrk811@gmail.com>
2026-03-25 20:44:25 +0900

    contentsのbefore_action設定

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,9 +1,51 @@
 class ContentsController < ApplicationController
+    before_action :authenticate_user!, except: :index
+    before_action :set_content, only: [:show, :edit, :update, :destroy]
+    #コンテンツ検索結果表示画面
     def index
         @contents = Content.all
         if params[:keyword].present?
             #contentモデルのtitle/genre/authorカラムを対象にあいまい検索。
             @contents = @contents.search_all_columns(params[:keyword])
         end
+    end
+
+    #コンテンツ新規登録画面
+    def new
+        @content = current_user.contents.build
+    end
+
+    #↑の登録処理
+    def create
+        @content = current_user.contents.build(content_params)
+        if @content.save
+            flash[:notice] = "コンテンツを登録しました"
+            redirect_to content_path(@content)
+        else
+            render "contents/show"
+    end
+
+    #コンテンツ一覧から飛ぶコメント閲覧画面へ
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    #################################################################################################
+    private
+
+    def content_params
+        params.require(:content).permit( :title, :detail, :genre, :manufacturer, :author )
+    end
+
+    def set_content
+        @content = Content.find(params[:id])
     end
 end
