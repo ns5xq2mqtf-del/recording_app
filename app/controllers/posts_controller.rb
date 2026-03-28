0313641a84dc708ec43838bb590a5948f38b5e28
ns5xq2mqtf-del <tkhsyrk811@gmail.com>
2026-03-29 05:11:09 +0900

    posts new画面作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,7 @@
 class PostsController < ApplicationController
+    def new
+        @content = Content.find(params[:content_id])
+        @post = current_user.posts.build
+        @post.content = @content
+    end
 end
